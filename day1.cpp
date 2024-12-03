28b4ac7323a2672d532e139751692527f88c61d2
Joshua Kim <jkim043@yahoo.com>
2024-12-02 19:45:47 -0500

    tab issue

--- day1.cpp
+++ day1.cpp
@@ -10,52 +10,52 @@ using namespace std;
 bool minsort(int i, int j) { return (i < j); }
 
 int main() {
-	vector<int> row1, row2;
-	int entry;
-	int maxdiff;
+    vector<int> row1, row2;
+    int entry;
+    int maxdiff;
 
-	string filename = "day1_input.txt";
+    string filename = "day1_input.txt";
 
-	ifstream infile(filename);
+    ifstream infile(filename);
 
-	// part 1
-	int index = 0;
-	while (infile >> entry) {
-		if (index % 2 == 0) {
-			row1.emplace_back(entry);
-		} else {
-			row2.emplace_back(entry);
-		}
+    // part 1
+    int index = 0;
+    while (infile >> entry) {
+        if (index % 2 == 0) {
+            row1.emplace_back(entry);
+        } else {
+            row2.emplace_back(entry);
+        }
 
-		index++;
-	}
+        index++;
+    }
 
-	sort(row1.begin(), row1.end(), minsort);
-	sort(row2.begin(), row2.end(), minsort);
+    sort(row1.begin(), row1.end(), minsort);
+    sort(row2.begin(), row2.end(), minsort);
 
-	for (int i = 0; i < (index / 2); i++) {
-		maxdiff += abs(row1[i] - row2[i]);
-	}
-	
-	cout << "Maximum difference: " << maxdiff << endl;
+    for (int i = 0; i < (index / 2); i++) {
+        maxdiff += abs(row1[i] - row2[i]);
+    }
+    
+    cout << "Maximum difference: " << maxdiff << endl;
 
-	// part 2
-	unordered_map<int,int> counts;
-	long long int similarity = 0;
+    // part 2
+    unordered_map<int,int> counts;
+    long long int similarity = 0;
 
-	for (int i = 0; i < (index / 2); i++) {
-		if (counts.find(row1[i]) == counts.end()) {
-			int hits = 0;
-			for (int j = 0; j < (index / 2); j++) {
-				if (row2[j] == row1[i]) { hits += 1; }
-			}
-			counts[row1[i]] = hits;
-		}
+    for (int i = 0; i < (index / 2); i++) {
+        if (counts.find(row1[i]) == counts.end()) {
+            int hits = 0;
+            for (int j = 0; j < (index / 2); j++) {
+                if (row2[j] == row1[i]) { hits += 1; }
+            }
+            counts[row1[i]] = hits;
+        }
 
-		similarity += (long long int)(counts[row1[i]] * row1[i]);
-	}
+        similarity += (long long int)(counts[row1[i]] * row1[i]);
+    }
 
-	cout << "Similarity score: " << similarity << endl;
-	return 0;
+    cout << "Similarity score: " << similarity << endl;
+    return 0;
 }
 
--- day2.cpp
+++ day2.cpp
@@ -7,78 +7,78 @@
 using namespace std;
 
 bool check_report(vector<int> report) {
-	int size = (int)report.size();
-	if (size <= 1) { return false; } 
+    int size = (int)report.size();
+    if (size <= 1) { return false; } 
 
-	int current = report[0];
-	bool increasing = (report[1] > report[0]);
+    int current = report[0];
+    bool increasing = (report[1] > report[0]);
 
-	for (int i = 1; i < size; ++i) {
-		if (abs(current - report[i]) > 3 || abs(current - report[i]) < 1) {
-			return false;	
-		}
-		if (increasing ^ (report[i] > current)) {
-			return false;
-		}
-		current = report[i];
-	}
+    for (int i = 1; i < size; ++i) {
+        if (abs(current - report[i]) > 3 || abs(current - report[i]) < 1) {
+            return false;   
+        }
+        if (increasing ^ (report[i] > current)) {
+            return false;
+        }
+        current = report[i];
+    }
  
-	return true;
+    return true;
 }
 
 vector<int> remove_one(vector<int> vec, int i) {
-	vec.erase(vec.begin() + i);
-	return vec;
+    vec.erase(vec.begin() + i);
+    return vec;
 }
 
 bool check_report_lenient(vector<int> report) {
-	int size = (int)report.size();
-	if (size <= 1) { return false; }
+    int size = (int)report.size();
+    if (size <= 1) { return false; }
 
-	int current = report[0];
-	bool increasing = (report[1] > report[0]);
+    int current = report[0];
+    bool increasing = (report[1] > report[0]);
 
-	for (int i = 1; i < size; ++i) {
-		// if failing, remove current level or previous
-		bool failure_case = false;
-		for (int j = i; j >= 0; j--) {
-			failure_case = failure_case || check_report(remove_one(report, j));
-		}
+    for (int i = 1; i < size; ++i) {
+        // if failing, remove current level or previous
+        bool failure_case = false;
+        for (int j = i; j >= 0; j--) {
+            failure_case = failure_case || check_report(remove_one(report, j));
+        }
 
-		if (abs(current - report[i]) > 3 || abs(current - report[i]) < 1) {
-			return failure_case;
-		}
-		if (increasing ^ (report[i] > current)) {
-			return failure_case;
-		}
-		current = report[i];
-	}
+        if (abs(current - report[i]) > 3 || abs(current - report[i]) < 1) {
+            return failure_case;
+        }
+        if (increasing ^ (report[i] > current)) {
+            return failure_case;
+        }
+        current = report[i];
+    }
 
-	return true;
+    return true;
 }
 
 int main() {
-	string filename = "day2_input.txt";
-	ifstream infile(filename);
+    string filename = "day2_input.txt";
+    ifstream infile(filename);
 
-	int passes = 0;
-	if (infile.good()) {
-		string line;
-		while (getline(infile, line)) {
-			istringstream ss(line);
-			int entry;
-			vector<int> report = {};
-			while (ss >> entry) {
-				report.push_back(entry);
-			}
-			// uncomment if part 1:
-			// if (check_report(report)) { passes++; }
-			if (check_report_lenient(report)) { passes++; }
-		}
-	}
-	
-	cout << "Number of good reports: " << passes << endl;
-	
-	return 0;
+    int passes = 0;
+    if (infile.good()) {
+        string line;
+        while (getline(infile, line)) {
+            istringstream ss(line);
+            int entry;
+            vector<int> report = {};
+            while (ss >> entry) {
+                report.push_back(entry);
+            }
+            // uncomment if part 1:
+            // if (check_report(report)) { passes++; }
+            if (check_report_lenient(report)) { passes++; }
+        }
+    }
+    
+    cout << "Number of good reports: " << passes << endl;
+    
+    return 0;
 }
 
